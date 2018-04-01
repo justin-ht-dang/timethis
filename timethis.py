7f5e26ed23324ecee1e77db0e73d516daa543f91
Justin Dang <justin@justind.me>
2018-04-01 15:50:43 +1000

    add functions to time arbitary code snippet

--- timethis.py
+++ timethis.py
@@ -1,22 +1,45 @@
 from time import time
+from collections import defaultdict
 import atexit
 
-total_time = {}
+total_time = defaultdict(lambda: 0)
+timing = {}
 
 def timethis(f):
     def wrap(*args, **kwargs):
         start = time()
         ret = f(*args)
         end = time()
-        if f.__name__ in total_time:
-            total_time[f.__name__] += end - start
-        else:
-            total_time[f.__name__] = end - start
+        
+        total_time[f.__name__ + "()"] += (end - start)
+
         return ret
+
     return wrap
 
 
+def timefrom(id):
+    if id in timing:
+        raise SyntaxError("Missing timeto function for id: " + id)
+    else:
+        start = time()
+        timing[id] = start
+    
+        
+def timeto(id):
+    if id not in timing:
+        raise SyntaxError("Missing timefrom function for id: " + id)
+    else:
+        end = time()
+        total_time[id] += (end - timing[id])
+        del timing[id]
+
+
 def report():
+    if timing:
+        ids = list(timing.keys()).__str__()
+        raise SyntaxError("Missing timeto function for id: " + ids)
+
     header = "-----------------------------------\n"\
     + "|             Report              |\n"\
     + "-----------------------------------"
